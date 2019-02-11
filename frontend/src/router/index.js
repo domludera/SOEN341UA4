90d534c161dc13ba74fad7ccc184b4b6e8c89604
HannaMichael <46824975+HannaMichael@users.noreply.github.com>
2019-02-11 11:22:18 -0500

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import LoginComponent from '@/components/login.vue'
 import SecureComponent from '@/components/secure.vue'
+import homeComponent from '@/components/home.vue'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: LoginComponent
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: homeComponent
     },
     {
       path: '/secure',
