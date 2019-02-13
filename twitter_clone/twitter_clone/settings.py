ea377b8e5262a97e834dfb3209d9188b3b21a07e
unknown <kevyn.bele.binda@gmail.com>
2019-02-13 11:01:36 -0500

    Merged from Alex's connectBackend branch

--- twitter_clone/twitter_clone/settings.py
+++ twitter_clone/twitter_clone/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'twitter_clone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,3 +131,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/admin'
--- twitter_clone/twitter_clone/urls.py
+++ twitter_clone/twitter_clone/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
     path('twitter/', include('twitter_clone_app.urls')),
     path('admin/', admin.site.urls),
