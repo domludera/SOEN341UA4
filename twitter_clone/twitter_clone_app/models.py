c65994aa7085ea03334d95e3fa3ca7e33c0e4832
Alec Adub <adub.alec@gmail.com>
2019-02-20 23:19:04 -0500

    makemigrations
    run command to make migrations, reference issue #22

--- twitter_clone/twitter_clone_app/models.py
+++ twitter_clone/twitter_clone_app/models.py
@@ -1,12 +1,8 @@
 from django.db import models
+from django.contrib.auth.admin import User
 
 
-class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-
-
-class Tweet(models.Model):
+class Chirp(models.Model):
     tweet_text = models.CharField(max_length=1000)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_posted = models.DateTimeField(auto_now_add=True)
