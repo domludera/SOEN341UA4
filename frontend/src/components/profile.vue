f0a68b1e049169937f7783136645ab5bc5611ac3
HannaMichael <46824975+HannaMichael@users.noreply.github.com>
2019-02-21 22:36:06 -0500

    Create profile.vue

---
+++ frontend/src/components/profile.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="profile">
+    <div class="container">
+    	<div class="row">
+    		<div class="col-sm-3">
+    			<div class="panel panel-default">
+    				<div class="panel-body">
+    					<a href="#"><img class="img-responsive" alt="Picture" src=""></a>
+    					<div class="row">
+    						<div class="col-xs-3">
+    							<h5>
+    								<small>Tweets</small>
+    								<a>60</a>
+    							</h5>
+    						</div>
+    						<div class="col-xs-4">
+    							<h5>
+    								<small>Following</small>
+    								<a>70</a>
+    							</h5>
+    						</div><br>
+    						<div class="col-xs-5">
+    							<h5>
+    								<small>Followers</small>
+    								<a>90</a>
+    							</h5>
+    						</div>
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+			</div>
+    </div>
+  </div>
+</template>
