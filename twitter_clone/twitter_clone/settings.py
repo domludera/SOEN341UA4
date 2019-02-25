f582ff7b3a5d755492f1d578e6274b9cfc8ed8e2
domludera <31933091+domludera@users.noreply.github.com>
2019-02-24 20:37:56 -0500

    conversion

--- twitter_clone/twitter_clone/settings.py
+++ twitter_clone/twitter_clone/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'twitter_clone_app',
+    'webpack_loader',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'twitter_clone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -84,12 +84,6 @@ DATABASES = {
         'PASSWORD': 'root',
         'HOST': '35.238.157.110',
         'PORT': '5432',
-        'OPTIONS': {
-            'sslmode': 'verify-ca',
-            'sslrootcert': os.environ['SERVER_CA_341'],
-            "sslcert": os.environ['CLIENT_CERT_341'],
-            "sslkey": os.environ['CLIENT_KEY_341'],
-        }
     }
 }
 
@@ -130,6 +124,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/public/'
 
-LOGIN_REDIRECT_URL = '/twitter/home'
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'dist'),
+)
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'CACHE': not DEBUG,
+        'BUNDLE_DIR_NAME': '',
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
+        'POLL_INTERVAL': 0.1,
+        'TIMEOUT': None,
+        'IGNORE': ['.+\.hot-update.js', '.+\.map']
+    }
+}
+
+#LOGIN_REDIRECT_URL = '/twitter/home'
--- twitter_clone/twitter_clone_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.twitter, name='twitter'),
-    path('home/', views.home, name='home'),
-]
