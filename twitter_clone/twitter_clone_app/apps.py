2621ea13c1cece1e715209623dff3e605b342be0
Alec Adub <adub.alec@gmail.com>
2019-03-03 14:42:04 -0500

    Clear twitter_clone
    Problem with all the merging, need to clear the folder before merging backendPostTweet inside the master.

--- twitter_clone/twitter_clone_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TwitterCloneAppConfig(AppConfig):
-    name = 'twitter_clone_app'
--- twitter_clone/twitter_clone_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.twitter, name='twitter'),
-    path('home/', views.home, name='home'),
-    path('registration/', views.registration, name='registration'),
-]
