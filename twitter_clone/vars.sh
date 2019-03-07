3d03fc3c22a2a2c0822e8840cabd8d6d5ca3efb2
Dominik Ludera <domludera@gmail.com>
2019-03-06 19:09:12 -0500

    t

--- twitter_clone/vars.sh
+++ twitter_clone/vars.sh
@@ -1,6 +1,6 @@
 #!/usr/env/bin/bash
 
 
-export SERVER_CA_341="./keys/server-ca.pem"
-export CLIENT_KEY_341="./keys/client-key.pem"
-export CLIENT_CERT_341="./keys/client-cert.pem"
+export SERVER_CA_341="./server-ca.pem"
+export CLIENT_KEY_341="./client-key.pem"
+export CLIENT_CERT_341="./client-cert.pem"
