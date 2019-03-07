e866c015cac52623e6a6c1ec394db22461a9d56d
Alec Adub <adub.alec@gmail.com>
2019-03-06 21:29:17 -0500

    Add user login test
    Full user login test. this tackle issue #

--- twitter_clone/twitter_clone_app/tests.py
+++ twitter_clone/twitter_clone_app/tests.py
@@ -1,6 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        self.assertEqual(1 + 1, 2)
+class UserLogin(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testUser',
+            'password': 'alecadub'
+        }
+        User.objects.create_user(**self.credentials)
+
+    def test_login(self):
+        response = self.client.post('/twitter/login/', self.credentials, follow=True)
+        self.assertTrue(response.context['user'].is_active)
+
