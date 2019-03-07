8d882db382e19ff81fd60cf5571381160b30547a
domludera <31933091+domludera@users.noreply.github.com>
2019-03-07 18:04:02 -0500

    update settings.py

--- twitter_clone/twitter_clone/settings.py
+++ twitter_clone/twitter_clone/settings.py
@@ -38,6 +38,22 @@ if os.getenv('BUILD_ON_TRAVIS', None):
             'HOST': '127.0.0.1',
         }
     }
+
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [os.path.join(BASE_DIR, 'templates')],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+    ]
 else:
         # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
