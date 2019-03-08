8cd21448267ada026f8f39a44328d6506e4b06c0
Dominik Ludera <domludera@gmail.com>
2019-03-08 02:55:58 -0500

    t

--- twitter_clone/twitter_clone/settings.py
+++ twitter_clone/twitter_clone/settings.py
@@ -37,6 +37,18 @@ if os.getenv('BUILD_ON_TRAVIS', None):
         'django.contrib.staticfiles',
     ]
 
+    
+    MIDDLEWARE = [
+        'django.middleware.security.SecurityMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
+
+
     ROOT_URLCONF = 'twitter_clone.urls'
 
     TEMPLATES = [
