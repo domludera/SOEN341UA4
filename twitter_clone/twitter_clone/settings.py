f8db72545fbeb63371bd9a68ee705fa3bb58d7a3
Dominik Ludera <domludera@gmail.com>
2019-03-08 12:39:25 -0500

    t

--- twitter_clone/twitter_clone/settings.py
+++ twitter_clone/twitter_clone/settings.py
@@ -37,9 +37,38 @@ if os.getenv('BUILD_ON_TRAVIS', None):
         'django.contrib.staticfiles',
     ]
 
+    MIDDLEWARE = [
+        'django.middleware.security.SecurityMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
+
     ROOT_URLCONF = 'twitter_clone.urls'
 
+
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            DIRS = ['templates']
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+    ]
+
     DIRS = ['./templates']
+
+    STATIC_URL = 'static/'
 
 else:
         # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
