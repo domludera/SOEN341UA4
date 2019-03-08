84ebbdbc0d2af6baa711708e7ebf8f2a8a321034
Dominik Ludera <domludera@gmail.com>
2019-03-08 02:46:27 -0500

    t

--- twitter_clone/twitter_clone/settings.py
+++ twitter_clone/twitter_clone/settings.py
@@ -42,7 +42,7 @@ if os.getenv('BUILD_ON_TRAVIS', None):
     TEMPLATES = [
         {
             'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': [os.path.join(BASE_DIR, 'templates')],
+            'DIRS': ['templates'],
             'APP_DIRS': True,
             'OPTIONS': {
                 'context_processors': [
