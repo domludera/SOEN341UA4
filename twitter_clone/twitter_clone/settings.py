d109fa5a6c76029cf95c1a4f35bd2148859e29a6
Dominik Ludera <31933091+domludera@users.noreply.github.com>
2019-03-14 17:54:39 -0400

    Revert "Merge UnitTestMainFeatures into master"

--- twitter_clone/twitter_clone/settings.py
+++ twitter_clone/twitter_clone/settings.py
@@ -13,66 +13,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 if os.getenv('BUILD_ON_TRAVIS', None):
-
-    # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-    # Quick-start development settings - unsuitable for production
-    # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-    # SECURITY WARNING: keep the secret key used in production secret!
     SECRET_KEY = 'gmfddk@^5%2)v6gku#^s&jjqt=wp%k)2gjtl=dnf)z29+u05w-'
-
-    # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = False
-
-    ALLOWED_HOSTS = []
-
-    # Application definition
-
-    INSTALLED_APPS = [
-        'twitter_clone_app',
-        'django.contrib.admin',
-        'django.contrib.auth',
-        'django.contrib.contenttypes',
-        'django.contrib.sessions',
-        'django.contrib.messages',
-        'django.contrib.staticfiles',
-    ]
-
-    MIDDLEWARE = [
-        'django.middleware.security.SecurityMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    ]
-
-    ROOT_URLCONF = 'twitter_clone.urls'
-
-    TEMPLATES = [
-        {
-            'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': [os.path.join(BASE_DIR, 'templates')],
-            'APP_DIRS': True,
-            'OPTIONS': {
-                'context_processors': [
-                    'django.template.context_processors.debug',
-                    'django.template.context_processors.request',
-                    'django.contrib.auth.context_processors.auth',
-                    'django.contrib.messages.context_processors.messages',
-                ],
-            },
-        },
-    ]
-
-    WSGI_APPLICATION = 'twitter_clone.wsgi.application'
-
-    # Database
-    # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+    TEMPLATE_DEBUG = True
 
     DATABASES = {
         'default': {
@@ -83,49 +26,11 @@ if os.getenv('BUILD_ON_TRAVIS', None):
             'HOST': '127.0.0.1',
         }
     }
-    TEMPLATE_DEBUG = True
-
-    # Password validation
-    # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
-
-    AUTH_PASSWORD_VALIDATORS = [
-        {
-            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-        },
-    ]
-
-    # Internationalization
-    # https://docs.djangoproject.com/en/2.1/topics/i18n/
-
-    LANGUAGE_CODE = 'en-us'
-
-    TIME_ZONE = 'UTC'
-
-    USE_I18N = True
-
-    USE_L10N = True
-
-    USE_TZ = True
-
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-    STATIC_URL = '/static/'
-
-    LOGIN_REDIRECT_URL = '/twitter/home'
 else:
-    # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+        # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
     # Quick-start development settings - unsuitable for production
     # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -133,10 +38,12 @@ else:
     # SECURITY WARNING: keep the secret key used in production secret!
     SECRET_KEY = 'gmfddk@^5%2)v6gku#^s&jjqt=wp%k)2gjtl=dnf)z29+u05w-'
 
+
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = True
 
     ALLOWED_HOSTS = []
+
 
     # Application definition
 
@@ -179,6 +86,7 @@ else:
     ]
 
     WSGI_APPLICATION = 'twitter_clone.wsgi.application'
+
 
     # Database
     # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
@@ -200,6 +108,7 @@ else:
         }
     }
 
+
     # Password validation
     # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
@@ -218,6 +127,7 @@ else:
         },
     ]
 
+
     # Internationalization
     # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -230,6 +140,7 @@ else:
     USE_L10N = True
 
     USE_TZ = True
+
 
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/2.1/howto/static-files/
