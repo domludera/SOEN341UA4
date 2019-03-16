8087fc359b7dde247f59f48d463ea594e9b35a65
domludera <31933091+domludera@users.noreply.github.com>
2019-03-15 20:11:58 -0400

    updated form.py and profile page

--- twitter_clone/twitter_clone_app/forms.py
+++ twitter_clone/twitter_clone_app/forms.py
@@ -5,7 +5,7 @@ from .models import Chirp
 # Create a form, when this form will be POST, it will be possible to update the model associated with it.
 
 class HomeForm(forms.ModelForm):
-    tweet_text = forms.CharField(label='', required=False)
+    tweet_text = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'What\'s happening?'}))
 
     class Meta:
         model = Chirp  # Model associated with the form.
